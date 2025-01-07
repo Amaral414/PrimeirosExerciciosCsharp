ff667722c921b36818b507a3a3213bc25c0df68f
Gabriel Amaral <131305606+Amaral414@users.noreply.github.com>
2025-01-07 20:20:19 -0300

    Initial Commit

---
+++ PrimeiroProjetoUdemy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeiroProjetoUdemy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Funfo");
+            Console.WriteLine("Funfo");
+        }
+    }
+}
